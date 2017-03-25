74a8ca3d9a6d220ca4e9662b21bbcb8ade11db27
m <sipnot2mail.ru>
2017-03-26 01:10:43 +0300

    Initial commit

---
+++ Net.java
@@ -0,0 +1,112 @@
+import sun.management.Sensor;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * Created by m on 19.03.2017.
+ */
+public class Net {
+    public static void main(String args[]) {
+
+
+        try (ServerSocket ss = new ServerSocket(44441)) {
+
+             new Http_client(44441);
+             new Http_server(ss.accept());
+
+
+
+        } catch (Exception e) {
+
+            System.out.println("Что-то пошло не так при создании сокета");
+        }
+
+
+
+//hhh
+    }
+}
+
+
+
+
+class Http_server extends Thread {
+    Socket socket;
+
+    Http_server(Socket s) {
+
+        socket =s;
+        setPriority(MAX_PRIORITY);
+        start();
+
+    }
+    public  void run() {
+
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+
+
+
+            String s = br.readLine();
+
+            System.out.println(s+"   воистенну");
+
+            String ss = br.readLine();
+
+            System.out.println(ss+"   ваще воистенну");
+
+        } catch (Exception e) {
+
+            System.out.println("Не удалось считать строку из клиента");
+        }
+
+
+        // System.out.println("Чупа-чупа");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+    class Http_client{
+    int port;
+
+    Http_client(int port){
+
+        this.port = port;
+
+        try (Socket socket = new Socket("localhost", port)) {
+
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            pw.println("Соседи пидары");
+
+            pw.println("Соседи пидары скоро сдохнут");
+            pw.flush();
+
+
+
+        } catch (Exception e) {
+
+            System.out.println("Не отправить строку из клиента");
+
+        }
+
+
+    }
+
+
+    }
+
+
+
+
+
+
