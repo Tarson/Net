7a6568c52ac2828cfb3824eed7e9a9d46670393b
User <sipnot@mail.ru>
2017-06-02 19:42:44 +0300

    1

--- Net.java
+++ Net.java
@@ -4,30 +4,33 @@ import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by m on 19.03.2017.
  */
 public class Net {
     public static void main(String args[]) {
+        new Http_client(44444);
+
+        try (ServerSocket ss = new ServerSocket(44444)) {
 
 
-        try (ServerSocket ss = new ServerSocket(44442)) {
 
-           new Http_client(44442);
-
-            System.out.println(" устаневливаем соединение");
+           System.out.println(" устанавливаем соединение");
              new Http_server(ss.accept());
 
 
 
         } catch (Exception e) {
 
-            System.out.println("Что-то пошло не так при создании сокета");
+           System.out.println("Что-то пошло не так при создании сокета");
         }
 
 
 
-//hhh
+
     }
 }
 
@@ -46,48 +49,76 @@ class Http_server extends Thread {
 
     }
     public  void run() {
-        System.out.println(" ждем  данные");
+        System.out.println(" Ждём запрос от клиента");
 
         try {
 
             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-
-            BufferedOutputStream pw = new BufferedOutputStream((socket.getOutputStream()));
-
+            //  BufferedOutputStream bos = new BufferedOutputStream((socket.getOutputStream()));
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
 
 
             ss = br.readLine();
 
-            System.out.println(ss);
 
 
-            if(ss.equals("hello")){
+
+            if(ss.equals("program")){
 
                 try
 
-                {// здесь адрес файла для выгрузки
-
-                  //  pw.println("10203040506070809");
-                    System.out.println("печатаем");
-
-                    File file = new File("d:/Blink.bin");
-                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-
-                    byte [] data = new byte[bis.available()];
-                    bis.read(data);
+                {  pw.println("ready");// Greetings with CLIENT
+                    System.out.println("hello from S");
 
 
-                    pw.write(data);
-                    pw.flush();
-                    System.out.println("отослали");
+
+
+                    // здесь адрес файла для загрузки
+
+                    int c;
+
+                    InputStream is = socket.getInputStream();
+
+                    List <Byte> buf = new ArrayList<>();
+
+
+                    while((c = is.read())!=-1)
+                    {
+
+                        buf.add((byte)c);
+                    }
+
+                    byte [] data = new byte[buf.size()];
+                    for(int i=0; i < data.length;i++)
+                    {
+
+                        data[i]=buf.get(i);
+
+                    }
+
+
+               File file = new File("d:BlinkIN.bin");
+
+
+                    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
+
+
+                    bos.write(data);
+                    bos.flush();
+
+
+
+                } catch (Exception e) {
+
+                    System.out.println("Не отправить строку из сервера");
 
                 }
 
-                catch (Exception e) {
 
-                    System.out.println("Не удалось отправить файл");
-                }
+
+
+
+
 
 
 
@@ -101,7 +132,7 @@ class Http_server extends Thread {
 
         } catch (Exception e) {
 
-            System.out.println("Не удалось считать строку из клиента");
+            System.out.println("Не удалось считать строку из сервера");
         }
 
 
@@ -128,6 +159,9 @@ class Http_server extends Thread {
 
     class Http_client extends Thread {
     int port;
+    String s;
+  String Greetings_from_S;
+
 
     Http_client(int port){
 
@@ -144,51 +178,85 @@ class Http_server extends Thread {
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
 
 
-                pw.println("hello");
+                pw.println("program");// Greetings with SERVER
+                System.out.println("hello from C");
+
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                Greetings_from_S = br.readLine();
+                //System.out.println(Greetings_from_S);
 
 
 
-                int c;
-
-                InputStream is = socket.getInputStream();
-
-                ArrayList <Byte> buf = new ArrayList<>();
 
 
-                while((c = is.read())!=-1)
-                {
 
-                    buf.add((byte)c);
+
+                if(Greetings_from_S.equals("ready")) {
+
+                    try
+
+                    {
+
+                        File file = new File("d:/blinkOUT.bin");
+                        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+
+
+
+                        byte [] data = new byte[bis.available()];
+                        bis.read(data);
+
+                        byte x = -127;
+                        byte [] data_buffer = new byte[1024];
+                        Arrays.fill(data_buffer, x);
+
+                        int frames = data.length/1024;
+                        int residy = data.length%1024;
+                        int count_frame=0;
+                        for (int i = 0; i < frames;i++) {
+
+
+                            for (int k = 0; k< (1024); k++) {
+                                data_buffer[k] = data[i];
+
+
+                            }
+
+
+
+                                BufferedOutputStream bos = new BufferedOutputStream((socket.getOutputStream()));
+
+
+                                bos.write(data);
+                                bos.flush();
+                                System.out.println("1024 байт отправлено");
+                                Arrays.fill(data_buffer, x);
+
+                            count_frame++;
+                        }
+
+
+
+
+                    } catch (Exception e) {
+
+                        System.out.println(e);
+
+                    }
+
+
                 }
 
-                byte [] data = new byte[buf.size()];
-                for(int i=0; i < data.length;i++)
-                {
 
-                    data[i]=buf.get(i);
+        } catch (Exception e) {
 
-                }
+            System.out.println("Не открылся сокет");
 
+        }
 
 
 
-                File file = new File("d:Blink2.bin");
-
-
-                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
-
-
-                 bos.write(data);
-                 bos.flush();
-
-
-
-
-            } catch (Exception e) {
-
-                System.out.println("Не отправить строку из клиента");
-
-            }
 
 
         }
