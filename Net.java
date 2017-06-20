947ea8243e815295c29bec24b1980c9a09033f56
User <sipnot@mail.ru>
2017-06-21 02:14:20 +0300

    1

--- Net.java
+++ Net.java
@@ -12,21 +12,21 @@ import java.util.List;
  */
 public class Net {
     public static void main(String args[]) {
-        new Http_client(44442);
+        new Http_client(4000);
 
-        try (ServerSocket ss = new ServerSocket(44442)) {
-
-
-
-           System.out.println(" устанавливаем соединение");
-             new Http_server(ss.accept());
+     //   try (ServerSocket ss = new ServerSocket(44441)) {
 
 
 
-        } catch (Exception e) {
+     //      System.out.println(" устанавливаем соединение");
+     //        new Http_server(ss.accept());
 
-           System.out.println("„то-то пошло не так при создании сокета");
-        }
+
+
+   //     } catch (Exception e) {
+
+    //       System.out.println("„то-то пошло не так при создании сокета");
+    //    }
 
 
 
@@ -35,9 +35,148 @@ public class Net {
 }
 
 
+class Http_client extends Thread {
+    int port;
+    String s;
+    String Greetings_from_S;
 
 
-class Http_server extends Thread {
+    Http_client(int port){
+
+        this.port = port;
+        start();
+
+
+
+    }
+    public  void run() {
+
+        try (Socket socket = new Socket("192.168.1.200", port)) {
+            //socket.setSendBufferSize(1024);
+            PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+            pw.println("program");// Greetings with SERVER
+            System.out.println("program");
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            Greetings_from_S = br.readLine();
+            System.out.println(Greetings_from_S);
+
+
+            if(Greetings_from_S.equals("ready")) {
+
+                try
+
+                {
+//BlinkOUT.bin
+                    File file = new File("d:BlinkOUT.bin");
+                    BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+
+
+
+                    byte [] data = new byte[bis.available()];
+                    bis.read(data);
+
+
+                    byte [] data_buffer = new byte[1024];
+
+
+                    int frames = data.length/1024;
+                    System.out.println(frames);
+                    int residy = data.length%1024;
+
+
+                    for (int i = 0; i < frames;i++) {
+                        for (int k = 0; k< (1024); k++) {
+                            data_buffer[k] = data[k+1024*(i)];
+                        }
+
+
+
+                       sendingChunk(data_buffer);
+
+                        //     while (br.readLine()!="ok");
+                        //     {}
+
+
+                    }
+                    byte [] data_buffer2= new byte[residy];
+                    for (int i = 0; i < residy;i++) {
+
+                        data_buffer2[i] = data[i+1024*(frames)];
+                    }
+
+
+
+                    sendingChunk(data_buffer2);
+
+                    pw.println("stop program");//
+                    System.out.println("stop program");
+
+
+
+               //     socket.close();
+
+
+                } catch (Exception e) {
+
+                    System.out.println(e);
+
+                }
+
+
+            }
+
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+
+
+
+    public void sendingChunk (byte [] data_buffer){
+        try (Socket socket = new Socket("192.168.1.200", 4001)){
+            BufferedOutputStream bos = new BufferedOutputStream((socket.getOutputStream()));
+            bos.write(data_buffer);
+            bos.flush();
+            //Thread.sleep(15000);
+
+            System.out.println(data_buffer.length);
+        }
+        catch (Exception e) {
+
+            System.out.println(e);
+
+        }
+
+    }
+
+    public void sendingOK (){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Http_server extends Thread {
     Socket socket;
     String ss;
 
@@ -97,7 +236,7 @@ class Http_server extends Thread {
                     }
 
                    // pw.println("ok");
-               File file = new File("d:BlinkIN.bin");
+               File file = new File("d:arm.bin");
 
 
                     BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file));
@@ -149,138 +288,6 @@ class Http_server extends Thread {
 
 
 }
-
-
-
-
-
-
-
-
-    class Http_client extends Thread {
-    int port;
-    String s;
-  String Greetings_from_S;
-
-
-    Http_client(int port){
-
-        this.port = port;
-        start();
-
-
-
-    }
-        public  void run() {
-
-            try (Socket socket = new Socket("192.168.1.35", port)) {
-
-                PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
-                pw.println("program");// Greetings with SERVER
-                System.out.println("hello from C");
-
-
-                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                Greetings_from_S = br.readLine();
-                //System.out.println(Greetings_from_S);
-
-
-                if(Greetings_from_S.equals("ready")) {
-
-                    try
-
-                    {
-
-                        File file = new File("d:/blinkOUT.bin");
-                        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-
-
-
-                        byte [] data = new byte[bis.available()];
-                        bis.read(data);
-
-
-                        byte [] data_buffer = new byte[1024];
-
-
-                        int frames = data.length/1024;
-                        System.out.println(frames);
-                        int residy = data.length%1024;
-
-
-                        for (int i = 0; i < frames;i++) {
-                            for (int k = 0; k< (1024); k++) {
-                                data_buffer[k] = data[k+1024*(i)];
-                         }
-
-
-
-                         sendingChunk(socket,data_buffer);
-
-                       //     while (br.readLine()!="ok");
-                       //     {}
-
-
-                         }
-                        byte [] data_buffer2= new byte[residy];
-                        for (int i = 0; i < residy;i++) {
-
-                                data_buffer2[i] = data[i+1024*(frames)];
-                        }
-
-
-
-                        sendingChunk(socket,data_buffer2);
-
-
-
-
-
-                        socket.close();
-
-
-                    } catch (Exception e) {
-
-                        System.out.println(e);
-
-                    }
-
-
-                }
-
-
-        } catch (Exception e) {
-
-            System.out.println(e);
-
-        }
-
-     }
-
-
-
-
-         public void sendingChunk (Socket socket,byte [] data_buffer){
-            try {
-                BufferedOutputStream bos = new BufferedOutputStream((socket.getOutputStream()));
-                bos.write(data_buffer);
-                bos.flush();
-
-
-                {System.out.println("1024 байт файла отправлено");}
-            }
-            catch (Exception e) {
-
-                System.out.println(e);
-
-            }
-
-         }
-
-         public void sendingOK (){}
-
-
-
 
 
 
